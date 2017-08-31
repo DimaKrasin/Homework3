4032c18d7b505dbc27e505414489602814fd16c8
DimaKrasin <ktasindim@gmail.com>
2017-09-01 00:20:18 +0300

    Переделал 1

--- src/com/goit/homework/Homework3.java
+++ src/com/goit/homework/Homework3.java
@@ -1,10 +1,89 @@
 package com.goit.homework;
-    import java.util.ArrayList;
     import java.util.Arrays;
     import java.util.Scanner;
 
     public class Homework3 {
+
         public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("Введите длину массива: ");
+
+            int size = input.nextInt();
+            int array[] = new int[size];
+
+            System.out.println("Вводите массив: ");
+
+            try {
+                for (int i = 0; i < size; i++) {
+                    array[i] = input.nextInt();
+                }
+            }catch(Exception e){
+            System.out.println("Вы попытались записать недопустимое значение в массив");
+            }
+
+            magic(array);
+        }
+
+        public static void magic(int[] array){
+            for (int i = 0; i < array.length; i++) {
+                array[i] = Integer.valueOf(array[i]);
+            }
+
+            int max = array[0];
+            int min = array[0];
+            double num5 = 0;
+            int maxrepit = 0;
+            int minrepit = array.length ;
+
+            for (int i = 0; i < array.length; i++) {
+
+                if (array[i] > max) max = array[i];  //MIN
+                if (array[i] < min) min = array[i];  //MAX
+                if (array[i] == 5) ++num5;         //Repit of 5
+
+                for (int j = 0; j < i; j++) {
+                    if (array[j] > array[j + 1]) {
+                        int t = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = t;
+                    }
+                }
+
+                int counter = 0;
+                for(int c = 0; c < array.length; c++) {
+                    if (array[i] == array[c]) counter++;
+                }
+                maxrepit = maxrepit < counter ? counter : maxrepit;
+                minrepit = minrepit > counter ? counter : minrepit;
+            }
+
+            System.out.println("Ваш массив :" + Arrays.toString(array));
+            System.out.println("Самое большое число :" + max);
+            System.out.println("Самое маленькое число :" + min);
+            System.out.println("Число пять встречаеться " + num5 + " раз(а)");
+            System.out.println("Максимальное число повторений :" + maxrepit);
+            System.out.println("Минимальное число повторений :" + minrepit);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
             Scanner sc = new Scanner(System.in);
             ArrayList list = new ArrayList();
@@ -52,7 +131,5 @@ package com.goit.homework;
             System.out.println("Самое маленькое число :" + min);
             System.out.println("Число пять встречаеться " + num5 + " раз(а)");
             System.out.println("Максимальное число повторений :" + maxrepit);
-            System.out.println("Минимальное число повторений :" + minrepit);
-
-        }
-    }
+            System.out.println("Минимальное число повторений :" + minrepit;
+            */
